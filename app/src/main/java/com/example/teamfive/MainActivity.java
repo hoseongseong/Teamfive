b91180888e64a5d15285d995fccb8c70bee84845
hoseongseong <hsv1961@naver.com>
2021-09-25 23:45:43 +0900

    hoseong

--- app/src/main/java/com/example/teamfive/MainActivity.java
+++ app/src/main/java/com/example/teamfive/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.teamfive;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference db = firebaseDatabase.getReference();
+    TextView textview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textview = (TextView)findViewById(R.id.id);
+
+        db.child("User").child("test").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String string = snapshot.getValue(String.class);
+                textview.setText(string);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
     }
 }
