9c85a578107ddbb26d799609def47df1f34f98d4
jongin <jongin0121@naver.com>
2021-10-03 20:04:01 +0900

    .

--- app/src/main/java/com/example/teamfive/PlusFragment.java
+++ app/src/main/java/com/example/teamfive/PlusFragment.java
@@ -1,23 +1,60 @@
 package com.example.teamfive;
 
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class PlusFragment extends Fragment {
     private View view;
     private Context context;
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         context = getActivity();
         view = inflater.inflate(R.layout.plusfragment_layout, container, false);
+
+        view.getBackground().setAlpha(100);
+
+        //----------------------추가 버튼 다이얼로그
+        Button button1 = (Button)view.findViewById(R.id.plusButton);
+        button1.setOnClickListener(new View.OnClickListener(){
+          public void onClick(View view){
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setTitle("추가 하시겠습니까?")        // 제목 설정
+                        //.setMessage("메시지")        // 메세지 설정
+                        .setCancelable(false)        // 뒤로 버튼 클릭시 취소 가능 설정
+                        .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        })
+                        .setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        });
+
+                AlertDialog dialog = builder.create();    // 알림창 객체 생성
+                dialog.show();    // 알림창 띄우기
+            }
+        });
 
         return view;
     }
