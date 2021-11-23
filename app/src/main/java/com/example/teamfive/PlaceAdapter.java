53d1e0920f1577133058937062473314187098dd
hoseongseong <hsv1961@naver.com>
2021-11-23 19:04:05 +0900

    hoseong

---
+++ app/src/main/java/com/example/teamfive/PlaceAdapter.java
@@ -0,0 +1,107 @@
+package com.example.teamfive;
+
+import android.content.Context;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.ArrayList;
+
+public class PlaceAdapter extends RecyclerView.Adapter<PlaceAdapter.PlaceViewHolder> {
+
+    ArrayList<String> placelist;
+    Context context;
+
+    private FirebaseStorage storage;
+    private StorageReference storageReference;
+    private StorageReference submitProfile;
+
+    private FirebaseDatabase database=FirebaseDatabase.getInstance();
+    private DatabaseReference db=database.getReference();
+
+    String user_id;
+
+    public PlaceAdapter(ArrayList<String> placelist, Context context,String user_id) {
+        this.placelist = placelist;
+        this.context = context;
+        this.user_id = user_id;
+    }
+
+    @NonNull
+    @Override
+    public PlaceViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.placexml,parent,false);
+        PlaceAdapter.PlaceViewHolder holder = new PlaceAdapter.PlaceViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PlaceViewHolder holder, int position) {
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+
+
+        db.child("Itemlist").child(user_id).child(placelist.get(position)).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                PlaceItem item=snapshot.getValue(PlaceItem.class);
+                String plc_name=item.getName();
+                holder.name_place.setText(plc_name);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        submitProfile = storageReference.child(user_id+"/"+placelist.get(position));
+        submitProfile.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Glide.with(holder.img_place)
+                        .load(uri)
+                        .into(holder.img_place);
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                holder.name_place.setText(""+e);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return (placelist !=null ? placelist.size() : 0);
+    }
+
+    public class PlaceViewHolder extends RecyclerView.ViewHolder {
+        TextView name_place;
+        ImageView img_place;
+        public PlaceViewHolder(@NonNull View itemView) {
+            super(itemView);
+            this.name_place=itemView.findViewById(R.id.place_name);
+            this.img_place=itemView.findViewById(R.id.place_img);
+        }
+    }
+}
--- app/src/main/java/com/example/teamfive/listFragment.java
+++ app/src/main/java/com/example/teamfive/listFragment.java
@@ -1,24 +1,97 @@
 package com.example.teamfive;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class listFragment extends Fragment {
     private View view;
     private Context context;
+
+    ArrayList<String> placelist;
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+
+    private FirebaseAuth mFirebaseAuth;
+    String user_id;
+
+    private FirebaseDatabase database;
+    private DatabaseReference db;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
         context = getActivity();
         view = inflater.inflate(R.layout.listfragment_layout, container, false);
 
+        init();
+
+        upload();
+
         return view;
+    }
+
+    public void init() {
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        user_id=mFirebaseAuth.getCurrentUser().getUid();
+
+        placelist = new ArrayList();
+
+        database=FirebaseDatabase.getInstance();
+        db=database.getReference();
+
+        recyclerView = (view).findViewById(R.id.list_rcy);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(context);
+        recyclerView.setLayoutManager(layoutManager);
+        adapter = new PlaceAdapter(placelist,context,user_id);
+        recyclerView.setAdapter(adapter);
+
+    }
+
+    public void upload() {
+
+        db.child("Itemlist").child(user_id).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                placelist.clear();
+                try {
+                    for (DataSnapshot sn : snapshot.getChildren()) {
+                        placelist.add(sn.getKey());
+                        adapter.notifyDataSetChanged();
+                    }
+                } catch(NullPointerException e) {
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
     }
 }
